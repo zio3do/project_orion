cc2db69131b08724622490257824311cfb43ad9c
Zion Hadley <zionhadley@Sensei-Zs-MacBook-Pro.local>
2026-01-28 19:11:35 -0500

    first proof: commutative property of addition via basic natural number tactics

--- LeanPhase1.lean
+++ LeanPhase1.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `LeanPhase1` library.
 -- Import modules here that should be built as part of the library.
 import LeanPhase1.Basic
+import LeanPhase1.Practice
--- LeanPhase1/Basic.lean
+++ LeanPhase1/Basic.lean
@@ -5,4 +5,4 @@ def hello := "world"
 def add (a b : Nat) : Nat :=
   a + b
 
-theorem add_comm (a b : Nat) : add a b = add b a := by simp [add, Nat.add_comm]
+-- theorem add_comm (a b : Nat) : add a b = add b a := by simp [add, Nat.add_comm]
